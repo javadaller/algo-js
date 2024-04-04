51a7e290aa501c0ed9c9d05b18583f81d5a7baf2
javadaller <arnaudvanacker@yahoo.fr>
2024-04-04 12:27:12 +0200

    mieux fini...

--- exo1/app.js
+++ exo1/app.js
@@ -1,5 +1,17 @@
 (() => {
     // YOUR CODE HERE
-    const variable="Un contenu fort intéressant";
-    console.log(variable);
+    
 })();
+
+const text="Un contenu fort intéressant!";
+console.log(text);
+document.querySelector('#display1').innerHTML=text;
+
+
+const lName='Van Acker';
+const fName='Arnaud';
+const city='Orbais';
+const text2='Your name is '+fName+' '+lName+' and you live in '+city+'.';
+
+console.log(text2);
+document.querySelector('#display2').innerHTML=text2;
